db7c21f038bb06ff0e2fc06b5b7a6080a82fc3ab
jsun454 <jsun46@umail.ucsb.edu>
2019-09-03 19:39:53 -0700

    Populate scoreboard with dummy data

--- app/src/main/java/com/example/jeffrey/multicasttest3/MainActivity.kt
+++ app/src/main/java/com/example/jeffrey/multicasttest3/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.jeffrey.multicasttest3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.SimpleItemAnimator
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = GroupAdapter<ViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activity_main_rv_scoreboard.adapter = adapter
+        val animator = activity_main_rv_scoreboard.itemAnimator as SimpleItemAnimator
+        animator.supportsChangeAnimations = false
+
+        testPopulateScoreboard()
+        testUpdateScoreboard()
+    }
+
+    private fun testPopulateScoreboard() {
+        val names = arrayOf("Albert", "Bobby", "Carol", "Dennis", "Emily", "Felix", "Gary",
+            "Hannah", "Ian", "Jacob")
+        for(i in 1..10) {
+            val racer = Racer(names[i-1], 300, 300, 600, i, "Driving")
+            val racerItem = RacerItem(racer)
+            adapter.add(i-1, racerItem)
+        }
+    }
+
+    private fun testUpdateScoreboard() {
+
     }
 }
---
+++ app/src/main/java/com/example/jeffrey/multicasttest3/RacerItem.kt
@@ -0,0 +1,24 @@
+package com.example.jeffrey.multicasttest3
+
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.racer_row.view.*
+
+class RacerItem(val racer: Racer) : Item<ViewHolder>() {
+    override fun getLayout(): Int {
+        return R.layout.racer_row
+    }
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.apply {
+//            background.alpha = 0
+            racer_row_txt_name.text = racer.name
+            racer_row_txt_lap_time.text = racer.lapTime.toString()
+            racer_row_txt_best_lap.text = racer.bestLap.toString()
+            racer_row_txt_time_left.text = racer.timeLeft.toString()
+            racer_row_txt_kart_number.text = racer.kartNO.toString()
+            racer_row_txt_status.text = racer.status
+        }
+    }
+
+}
