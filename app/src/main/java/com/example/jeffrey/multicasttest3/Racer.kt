5a0f9737b5633f098b478e1a59b6645fe33fd559
jsun454 <jsun46@umail.ucsb.edu>
2019-09-03 18:10:21 -0700

    Added racer class

---
+++ app/src/main/java/com/example/jeffrey/multicasttest3/Racer.kt
@@ -0,0 +1,11 @@
+package com.example.jeffrey.multicasttest3
+
+// may have to change depending on what multicast needs
+class Racer(
+    val name: String,
+    val lapTime: Int,
+    val bestLap: Int,
+    val timeLeft: Int,
+    val kartNO: Int,
+    val status: String
+)
