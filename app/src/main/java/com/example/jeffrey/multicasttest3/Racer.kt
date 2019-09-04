828725b68cbdcb3cd2dae7246b5726a76a004639
jsun454 <jsun46@umail.ucsb.edu>
2019-09-03 21:32:57 -0700

    Made certain properties modifiable

--- app/src/main/java/com/example/jeffrey/multicasttest3/Racer.kt
+++ app/src/main/java/com/example/jeffrey/multicasttest3/Racer.kt
@@ -3,9 +3,9 @@ package com.example.jeffrey.multicasttest3
 // may have to change depending on what multicast needs
 class Racer(
     val name: String,
-    val lapTime: Int,
-    val bestLap: Int,
-    val timeLeft: Int,
+    var lapTime: Int,
+    var bestLap: Int,
+    var timeLeft: Int,
     val kartNO: Int,
-    val status: String
+    var status: String
 )
